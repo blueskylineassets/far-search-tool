4bf660fd06c9ecd61778eb208280d29c73e4f074
Daniel Chang <danielchang@macbookpro.mynetworksettings.com>
2025-12-26 23:38:58 -0500

    v0.1.2: Update URLs to point to public repo

--- far_search/__init__.py
+++ far_search/__init__.py
@@ -7,6 +7,6 @@ A LangChain tool for semantic search over Federal Acquisition Regulations (FAR).
 from far_search.tool import FARSearchTool
 from far_search.exceptions import FARSearchError, FARAPIError, FARRateLimitError
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["FARSearchTool", "FARSearchError", "FARAPIError", "FARRateLimitError"]
 
